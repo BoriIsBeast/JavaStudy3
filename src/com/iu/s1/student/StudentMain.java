9ee7116f0fc05a440b09e99adcef6caf67d6cac6
BoriIsBeast <wlsdn147@naver.com>
2022-01-04 23:16:59 +0900

    fix

--- src/com/iu/s1/student/StudentMain.java
+++ src/com/iu/s1/student/StudentMain.java
@@ -10,12 +10,9 @@ public class StudentMain {
 		
 		//make Student 호출
 		// 이름,번호,국어 영어,수학 출력
-		Student Student = new Student();
 		StudentUtil studentUtil = new StudentUtil();
-		
-		studentUtil.makeStudents();                       // 이부분 수정
-		Student [] students  = studentUtil.makeStudents();//이부분 수정
-		//Student student =studentUtil.makeStudent();
+		//Student student  = studentUtil.makeStudent();
+		Student [] students = studentUtil.makeStudents();
 		
 		for(int i = 0;i<students.length;i++) {
 		Student student = students[i];
